16d80343bd75cccd0e4ead1a4e7aeb95f69d9a41
SoolyChristy <402568286@qq.com>
2017-03-01 20:57:47 +0800

    Initial Commit

---
+++ SoolyWeatherTests/SoolyWeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  SoolyWeatherTests.swift
+//  SoolyWeatherTests
+//
+//  Created by SoolyChristina on 17/3/1.
+//  Copyright © 2017年 SoolyChristina. All rights reserved.
+//
+
+import XCTest
+@testable import SoolyWeather
+
+class SoolyWeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
